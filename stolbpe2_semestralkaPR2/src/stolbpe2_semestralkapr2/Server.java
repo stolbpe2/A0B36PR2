0600e661a68b0000444711c5101cd893cb6aaf94
Punk <Punk@147.32.218.109>
2013-03-05 21:35:58 +0100

    Prvni prace

---
+++ stolbpe2_semestralkaPR2/src/stolbpe2_semestralkapr2/Server.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stolbpe2_semestralkapr2;
+
+/**
+ *
+ * @author Punk
+ */
+public class Server {
+    
+}
---
+++ stolbpe2_semestralkaPR2/src/stolbpe2_semestralkapr2/Stolbpe2_semestralkaPR2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stolbpe2_semestralkapr2;
+
+/**
+ *
+ * @author Punk
+ */
+public class Stolbpe2_semestralkaPR2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
